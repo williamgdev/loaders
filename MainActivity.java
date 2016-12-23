37a6c22022b35c2a2ee8f7744fb956cf9076144d
William GT <william.devl@gmail.com>
2016-12-23 17:13:23 -0500

    Create UI to show Name from COnetnProvider

--- MainActivity.java
+++ MainActivity.java
@@ -2,12 +2,20 @@ package com.mac.fireflies.wgt.loaders;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textView = (TextView) findViewById(R.id.main_txt);
+    }
+
+    public void onClick(View view) {
+        
     }
 }
