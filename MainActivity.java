006eada860deadf5a27bf19fdaba9d73138c4b8d
William GT <william.devl@gmail.com>
2016-12-23 19:03:58 -0500

    implements LoadreCursor

--- MainActivity.java
+++ MainActivity.java
@@ -1,12 +1,19 @@
 package com.mac.fireflies.wgt.loaders;
 
-import android.support.v7.app.AppCompatActivity;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.CursorLoader;
+import android.support.v4.content.Loader;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<Cursor> {
     TextView textView;
+    private String TAG = "MainActivity => ";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +23,30 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void onClick(View view) {
-        
+        getSupportLoaderManager().initLoader(1, null, this);
+    }
+
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+        return new CursorLoader(
+                getApplicationContext(),
+                Uri.parse("content://com.mac.fireflies.wgt.clientscontentproviders/users"),
+                null, null, null, null);
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+        if (data != null && data.getCount() > 0){
+            data.moveToFirst();
+            do{
+                final String result = textView.getText().toString();
+                textView.setText(result + "\n" + data.getString(data.getColumnIndex("userName")));
+            }while (data.moveToNext());
+        }
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> loader) {
+        Log.d(TAG, "onLoaderReset: do Something");
     }
 }
